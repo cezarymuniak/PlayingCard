4419ca791a58aad1d157b60d4737eb7d35e3f6ba
CM <cezary.muniak@gmail.com>
2019-04-24 14:02:56 +0200

    Initial Commit

---
+++ PlayingCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayingCard
+//
+//  Created by Cez1 on 24/04/2019.
+//  Copyright © 2019 Cez1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
